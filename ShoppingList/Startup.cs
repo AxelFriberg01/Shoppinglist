930955fdd0396fa593619723889971f576d3d5a8
axelfriberg01 <axel.friberg.work@gmail.com>
2020-03-01 22:11:41 +0100

    Added readonly to deletebutton which disables highlighting+editing of text inside the button

--- ShoppingList/Startup.cs
+++ ShoppingList/Startup.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -36,7 +37,11 @@ namespace ShoppingList
             services.AddTransient<ProductsService>();
             //services.AddControllersWithViews().AddRazorRuntimeCompilation();
 
-            services.AddControllersWithViews();
+            services.AddControllersWithViews(o =>
+            {
+                o.Filters.Add(
+                    new AutoValidateAntiforgeryTokenAttribute());
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
